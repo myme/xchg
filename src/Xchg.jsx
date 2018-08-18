8939f98ad86702f72f72cd1c0dadf6af181533da
Martin Øinæs Myrseth <myrseth@gmail.com>
2018-08-18 23:50:21 +0200

    Add "End" button

--- src/Xchg.jsx
+++ src/Xchg.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
+  Button,
   CssBaseline,
   Paper,
+  Typography,
   withStyles,
 } from '@material-ui/core';
+import {
+  Close,
+} from '@material-ui/icons';
 
 import Qr from './Qr';
 
@@ -26,35 +31,44 @@ const styles = theme => ({
     alignItems: 'center',
     padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
   },
-  avatar: {
-    margin: theme.spacing.unit,
-    backgroundColor: theme.palette.secondary.main,
-  },
   form: {
     marginTop: theme.spacing.unit,
   },
-  submit: {
-    marginTop: theme.spacing.unit * 3,
-  },
 });
 
-function Xchg(props) {
+function Xchg(props, context) {
   const { classes, match } = props;
+  const { router } = context;
   const { uuid } = match.params;
   const url = global.location.toString();
+  const onEnd = () => {
+    router.history.push('/');
+  };
   return (
     <React.Fragment>
       <CssBaseline />
       <main className={classes.layout}>
         <Paper className={classes.paper}>
+          <Typography variant="headline">Press &quot;End&quot; to terminate the session.</Typography>
           <a href={url}>
             {uuid}
           </a>
-        </Paper>
-        <Paper className={classes.paper}>
+          <hr />
           <a href={url}>
             <Qr value={url} />
           </a>
+        </Paper>
+        <Paper className={classes.paper}>
+          <Typography variant="headline">Press &quot;End&quot; to terminate the session.</Typography>
+          <form className={classes.form} onSubmit={(ev) => { ev.preventDefault(); }}>
+            <Button
+              variant="fab"
+              color="secondary"
+              onClick={onEnd}
+            >
+              <Close />
+            </Button>
+          </form>
         </Paper>
       </main>
     </React.Fragment>
@@ -64,6 +78,12 @@ function Xchg(props) {
 Xchg.propTypes = {
   classes: PropTypes.object.isRequired,
   match: PropTypes.object.isRequired,
+};
+
+Xchg.contextTypes = {
+  router: PropTypes.shape({
+    history: PropTypes.object.isRequired,
+  }).isRequired,
 };
 
 export default withStyles(styles)(Xchg);
