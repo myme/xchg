aeaac8192477980ec62af090cb56f6f7bc689b0b
Martin Øinæs Myrseth <myrseth@gmail.com>
2018-08-19 12:01:47 +0200

    Add wss-server

---
+++ wss-server.js
@@ -0,0 +1,17 @@
+const WebSocket = require('ws');
+ 
+const wss = new WebSocket.Server({ port: 8080 });
+ 
+wss.on('connection', function connection(ws) {
+  console.log('new connection');
+  
+  ws.on('close', function dropped() {
+    console.log('client dropped');
+  })
+
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+ 
+  ws.send('something');
+});
